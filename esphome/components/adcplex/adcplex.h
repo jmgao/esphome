4e2d1ecac9e3d103afafd7ad6e56c482afe3e16a
Daniel Steen-Brungot <daniel.steenbrungot@gmail.com>
2020-09-10 14:03:59 +0200

    Update adcplex.h

--- esphome/components/adcplex/adcplex.h
+++ esphome/components/adcplex/adcplex.h
@@ -2,37 +2,39 @@
 
 #include "esphome/core/component.h"
 #include "esphome/components/sensor/sensor.h"
-// #include "esphome.h"
 
 namespace esphome {
 namespace adcplex {
-    
-/// Internal holder class that is in instance of Sensor so that the hub can create individual sensors.
-class ADCPLEXSensor : public sensor::ADCSensor {
+
+class ADCPlexSensor;
+  
+class ADCPLEXComponent : public Component {
     public:
-        ADCPLEXSensor(ADCPLEXComponent *paren) : parent_(parent) {}
-        void setup() override;
-        void update() override;
+        void register_sensor(ADCPLEXSensor *obj) { this->sensors_.push_back(obj); }
+        void dump_config() override;
+
+        void set_adc(sensor::ADCSensor *adc) { this->adc_ = adc; }
+        sensor::ADCSensor get_adc() { return this->adc_;}
+        float get_setup_priority() const override { return setup_priority::DATA; }
+
+    protected:
+        std::vector<ADCPLEXSensor *> sensors_;
+        sensor::ADCSensor *adc_;
+};
+  
+  class ADCPLEXSensor : public sensor::Sensor {
+    public:
+        ADCPLEXSensor(ADCPLEXComponent *parent) : parent_(parent) {}
         void set_power_pin(uint8_t power_pin) { this->power_pin = power_pin; }
-        uint8_t get_power_pin() const { return power_pin; }
         void set_delay(int delay) { this->delay = delay; }
+
+        uint8_t get_power_pin() const { return power_pin; }
         uint8_t get_delay() const { return delay; }
 
     protected:
         ADCPLEXComponent *parent_;
         uint8_t power_pin;
         int delay;
-};
-
-class ADCPLEXComponent : public Component {
-    public:
-        void register_sensor(ADCPLEXSensor *obj) { this->sensors_.push_back(obj); }
-        void dump_config() override;
-        /// HARDWARE_LATE setup priority
-        float get_setup_priority() const override { return setup_priority::DATA; }
-
-    protected:
-        std::vector<ADCPLEXSensor *> sensors_;
 };
 
 }  // namespace ADCPLEX
