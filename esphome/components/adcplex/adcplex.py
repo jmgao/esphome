fdf7391c3c46090e70853ab576a7aa3672675233
Daniel Steen-Brungot <daniel.steenbrungot@gmail.com>
2020-09-10 14:04:47 +0200

    Update adcplex.py

--- esphome/components/adcplex/adcplex.py
+++ esphome/components/adcplex/adcplex.py
@@ -1,27 +1,21 @@
   
+  
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor
-from esphome.const import CONF_POWER, CONF_ANALOG, CONF_DELAY, ICON_FLASH, UNIT_VOLT, CONF_ID
+from esphome.const import CONF_POWER, CONF_DELAY, ICON_FLASH, UNIT_VOLT, CONF_ID
 from . import  ADCPLEXComponent
 
 
-def validate_adc_pin(value):
-    vcc = str(value).upper()
-    if vcc == 'VCC':
-        return cv.only_on_esp8266(vcc)
-    return pins.analog_pin(value)
-
 DEPENDENCIES = ['ADCPLEX']
 
-ADCPLEXSensor = ADCPLEX_ns.class_('ADCPLEXSensor', sensor.ADCSensor)
+ADCPLEXSensor = ADCPLEX_ns.class_('ADCPLEXSensor', sensor.Sensor)
 
 CONF_ADCPLEX_ID = 'ADCPLEX_id'
 CONFIG_SCHEMA = sensor.sensor_schema(UNIT_VOLT, ICON_FLASH, 3).extend({
     cv.GenerateID(): cv.declare_id(ADCPLEXSensor),
     cv.GenerateID(CONF_ADCPLEX_ID): cv.use_id(ADCPLEXComponent),
     cv.Required(CONF_POWER): pins.output_pin,
-    cv.Required(CONF_ANALOG): validate_adc_pin,
     cv.Optional(CONF_DELAY, default=100): cv.integer,
 }).extend(cv.polling_component_schema('60s'))
 
@@ -34,6 +28,5 @@ def to_code(config):
 
     cg.add(var.set_power_pin(config[CONF_POWER]))
     cg.add(var.set_delay(config[CONF_DELAY]))
-    cg.add(var.set_pin(config[CONF_ANALOG]))
 
     cg.add(paren.register_sensor(var))
