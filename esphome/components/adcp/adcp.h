27dfe55f0c06e5414ae7e126fbaa3ac126a97a77
Ceiku <daniel.steenbrungot@gmail.com>
2020-09-15 22:33:36 +0000

    fixed some lint errors

--- esphome/components/adcp/adcp.h
+++ esphome/components/adcp/adcp.h
@@ -10,36 +10,33 @@ class ADCPSensor;
   
 class ADCPComponent : public Component {
 public:
-    void register_sensor(ADCPSensor *obj) { this->sensors_.push_back(obj); }
-    void update() override;
-    void dump_config() override;
+  void register_sensor(ADCPSensor *obj) { this->sensors_.push_back(obj); }
+  void update() override;
+  void dump_config() override;
 
-    void set_adc(sensor::ADCSensor *adc) { this->adc_ = adc; }
-    sensor::ADCSensor get_adc() { return this->adc_; }
+  void set_adc(sensor::Sensor *adc) { this->adc_ = adc; }
+  sensor::Sensor get_adc() { return this->adc_; }
 
-    float get_setup_priority() const override { return setup_priority::DATA; }
+  float get_setup_priority() const override { return setup_priority::DATA; }
 
 protected:
-    std::vector<ADCPSensor *> sensors_;
-    sensor::ADCSensor *adc_;
+  std::vector<ADCPSensor *> sensors_;
+  sensor::Sensor *adc_;
 };
 
 class ADCPSensor : public sensor::Sensor {
 public:
-    ADCPSensor(ADCPComponent *parent) : parent_(parent) {}
-    void set_power_pin(uint8_t power_pin) { this->power_pin = power_pin; }
-    void set_adc_id(std::string adc) { this->adc_ = adc; }
-    void set_delay(int delay) { this->delay = delay; }
+  ADCPSensor(ADCPComponent *parent) : parent_(parent) {}
+  void set_power_pin(uint8_t power_pin) { this->power_pin = power_pin; }
+  void set_delay(int delay) { this->delay = delay; }
 
-    uint8_t get_power_pin() const { return this->power_pin; }
-    std::string get_adc_id() const { return this->adc_; }
-    uint8_t get_delay() const { return this->delay; }
+  uint8_t get_power_pin() const { return this->power_pin; }
+  uint8_t get_delay() const { return this->delay; }
 
 protected:
-    ADCPComponent *parent_;
-    uint8_t power_pin;
-    std::string adc_;
-    int delay;
+  ADCPComponent *parent_;
+  uint8_t power_pin;
+  int delay;
 };
 }  // namespace adcp
 }  // namespace esphome
