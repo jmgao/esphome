1d24d650ca22064cf3b431fd14f8dc9840b67285
Rogan Dawes <rogan@dawes.za.net>
2021-08-19 17:08:46 +0200

    Provide the STM32 watchdog timer

---
+++ esphome/core/application_stm32.cpp
@@ -0,0 +1,14 @@
+#include "esphome/core/application.h"
+
+#ifdef ARDUINO_ARCH_STM32
+
+namespace esphome {
+
+static const char *const TAG = "app_stm32";
+
+void ICACHE_RAM_ATTR HOT Application::feed_wdt_arch_() {
+	// STM32 has no watchdog
+}
+
+}  // namespace esphome
+#endif
