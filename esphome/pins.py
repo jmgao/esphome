eaafdebd6d3fad13e9a9a0492e166e48b9f0573a
Rogan Dawes <rogan@dawes.za.net>
2021-08-19 15:48:06 +0200

    Fix typo in pins.py after adding genericSTM32 - squash this

--- esphome/pins.py
+++ esphome/pins.py
@@ -21,8 +21,8 @@ def _lookup_pin(value):
             board_pins_dict = boards.ESP32_BOARD_PINS
             base_pins = boards.ESP32_BASE_PINS
     elif CORE.is_ststm32:
-        board_pins_dict = boards.STM32_BOARD_PINS
-        base_pins = boards.STM32_BASE_PINS
+        board_pins_dict = boards.STSTM32_BOARD_PINS
+        base_pins = boards.STSTM32_BASE_PINS
     else:
         raise NotImplementedError
 
