ab1f8b74b344382f8a1631f29638ac18798f2cfa
Rogan Dawes <rogan@dawes.za.net>
2021-08-23 03:47:11 +0200

    STM32: fix lint warnings

--- esphome/core/application_stm32.cpp
+++ esphome/core/application_stm32.cpp
@@ -7,7 +7,7 @@ namespace esphome {
 static const char *const TAG = "app_stm32";
 
 void ICACHE_RAM_ATTR HOT Application::feed_wdt_arch_() {
-	// STM32 has no watchdog
+    // STM32 has no watchdog
 }
 
 }  // namespace esphome
